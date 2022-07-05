fe3f80e01f2c249bd782505c4e393d41d8c0f63e
Lusehair <56312971+lusehair@users.noreply.github.com>
2022-07-05 11:29:26 +0200

    need to finish last kata

---
+++ ml00/ex05/kata00.py
@@ -0,0 +1,4 @@
+t = (19,42,21)
+
+print("The " + str(len(t)) + " numbers are: ", end='')
+print(', '.join([str(i) for i in t]))
---
+++ ml00/ex05/kata02.py
@@ -0,0 +1,3 @@
+t = (3,30,2019,9,25) 
+
+print(str(t[3]).zfill(2) + '/' + str(t[4]).zfill(2) + '/' + str(t[2]) + ' ' + str(t[0]).zfill(2) + ':' + str(t[1]).zfill(2))
