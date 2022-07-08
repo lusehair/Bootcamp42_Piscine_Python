4ef1915851adc1709e7fa57ea78053adb67e94c8
Lucas SEHAIRI <lusehair@e1r6p18.42.fr>
2022-07-08 14:36:53 +0200

    change 0 on year print

--- ml00/ex05/kata02.py
+++ ml00/ex05/kata02.py
@@ -1,3 +1,3 @@
 t = (3,30,2019,9,25) 
 
-print(str(t[3]).zfill(2) + '/' + str(t[4]).zfill(2) + '/' + str(t[2]) + ' ' + str(t[0]).zfill(2) + ':' + str(t[1]).zfill(2))
+print(str(t[3]).zfill(2) + '/' + str(t[4]).zfill(2) + '/' + str(t[2]).zfill(4) + ' ' + str(t[0]).zfill(2) + ':' + str(t[1]).zfill(2))
