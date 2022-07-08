499c46e3192cddf4046b4b80b24f1afce81a3719
Lucas SEHAIRI <lusehair@e1r6p18.42.fr>
2022-07-08 13:36:49 +0200

    change for ex02

--- ml00/ex02/whois.py
+++ ml00/ex02/whois.py
@@ -1,14 +1,28 @@
 import sys 
 
-if len(sys.argv) == 2 :
-    print("AssertionError: more than one argument are provided")
+  
 
-elif sys.argv[1].isnumeric() != False :
-    print('AssertionError: argument is not integer')
 
-elif int(sys.argv[1]) % 2 != 0:
-    print("I'm odd")
-elif int(sys.argv[1]) == 0:
-    print("I'm Zero.") 
-else:
-    print("I'm Even.")
+
+
+if len(sys.argv) > 1 :
+
+    if len(sys.argv) != 2 :
+        print("AssertionError: more than one argument are provided")
+        exit()
+
+    num = sys.argv[1]; 
+
+    if(num.find('-') != -1) : 
+        num = num.lstrip('-')
+    if(num.find('+') != -1) :
+        num = num.lstrip('+')
+    if num.isnumeric() == False :
+        print('AssertionError: argument is not integer')
+
+    elif int(num)% 2 != 0:
+        print("I'm odd")
+    elif int(num) == 0:
+        print("I'm Zero.") 
+    else:
+        print("I'm Even.")
