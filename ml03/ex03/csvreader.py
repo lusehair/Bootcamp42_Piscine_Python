ef6172367181ae6806a62341385ed20e383ca8a8
Lucas Sehairi <lsehairi@eutelsat.com>
2022-10-03 12:03:15 +0200

    done with ex03

--- ml03/ex03/csvreader.py
+++ ml03/ex03/csvreader.py
@@ -2,7 +2,7 @@ import sys
 import csv 
 
 # https://zestedesavoir.com/tutoriels/954/notions-de-python-avancees/3-further/2-context-managers/ 
-
+# https://www.programiz.com/python-programming/csv
 
 class CsvReader():
     
@@ -13,56 +13,64 @@ class CsvReader():
     _skip_top = 0
     _skip_bottom = 0
     _sep = ','
+    _error = None 
     def __init__(self, filename=None, sep=',', header=False, skip_top=0, skip_bottom=0):
+    
+        if skip_bottom < 0 or skip_top < 0 :
+            self._error = "Skiper cannot be a negative number" 
+            return    
+        if filename is None :
+            self._error = "the filename is not specified"
+            return 
         self.filename = filename
         self.header = header
         self._skip_bottom = skip_bottom 
         self._skip_top = skip_top 
         self._sep = sep 
-        pass
-        # ... Your code here ...
     
     def __enter__(self):
-        with open(self.filename, 'r') as file: 
-            values = csv.reader(file) 
-
-        # ... Your code here ...
-        pass 
-    
-        #f.close()
-    
-    
+        try:
+            with open(self.filename, 'r') as file: 
+                reader = csv.reader(file, delimiter = self._sep) 
+                self.values = list(reader) 
+                for row in self.values : 
+                    for value in row :
+                        if value == "" :
+                            raise Exception("Error Data corrupted")
+        except: 
+            return None
+        return self
+        
     def __exit__(self, type, value, traceback): 
-        pass 
-    # ... Your code here ...
+        if type is not None :
+            print("here")
+            return None 
+        if self._error is not None :
+            return self._error 
+    
     def getdata(self):
 
         """ Retrieves the data/records from skip_top to skip bottom.
         Return:
         nested list (list(list, list, ...)) representing the data.
         """
+        ret = [] 
+        self._skip_bottom = len(self.values)  - (self._skip_bottom)
+        for i, value in enumerate(self.values) : 
+            if i > self._skip_top and i != self._skip_bottom and i != 0 :
+                ret.append(value)
+        return ret
 
-    # ... Your code here ...
-
-    
-    
     def getheader(self):
-        if self.header is True :
-            return self.values[0] 
-        else : 
-            return None 
-        
-
         """ Retrieves the header from csv file.
         Returns:
         list: representing the data (when self.header is True).
         None: (when self.header is False).
-        """
-
-
-# ... Your code here ...
-
-
+        """    
+        ret = None 
+        if self.header is True :
+            ret =  self.values[0]      
+        return ret 
 
 if __name__ == "__main__":
     filename = sys.argv[1]
