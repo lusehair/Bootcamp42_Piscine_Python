f4559078f69febc00c2a62658f413091fdae0eca
Lucas Sehairi <lsehairi@eutelsat.com>
2022-10-06 16:06:38 +0200

    ScrapBooker done, need to test mosaic

--- ml03/ex02/ScrapBooker.py
+++ ml03/ex02/ScrapBooker.py
@@ -4,8 +4,21 @@ class ScrapBooker() :
 
     ## CROP 
     # https://www.geeksforgeeks.org/how-to-crop-an-image-using-the-numpy-module/ 
-    # 
+    # https://stackoverflow.com/questions/39424052/how-to-set-coordinates-when-cropping-an-image-with-pil
 
+    ## THIN 
+        # https://numpy.org/doc/stable/reference/generated/numpy.delete.html 
+        # https://note.nkmk.me/en/python-numpy-delete/ 
+
+    ## JUXTAPOSITION 
+        # https://numpy.org/doc/stable/reference/generated/numpy.concatenate.html
+
+    ## Mosaic 
+        # https://numpy.org/doc/stable/reference/generated/numpy.tile.html
+        # 
+
+    ## Error Management 
+     # https://stackoverflow.com/questions/12569452/how-to-identify-numpy-types-in-python
 
     def crop(self, array, dim, position=(0,0)):
         """
@@ -23,7 +36,16 @@ class ScrapBooker() :
         Raise:
         ------
         """ 
-        pass 
+        if (type(array).__module__ == np.__name__) is False :
+            return None
+
+        if dim[0] > array.shape[0] or dim[1] > array.shape[1] :
+            return None 
+        if dim[0] < 0 or dim[1] < 0 or position[0] < 0 or position[1] < 0 : 
+            return None 
+        if position[0] > array.shape[0] or position[1] > array.shape[1] : 
+            return None 
+        return array[position[0]:dim[0] + position[0] , position[1]:dim[1] + position[1]]
 
     def thin(self, array, n, axis):
         """
@@ -42,8 +64,16 @@ class ScrapBooker() :
         ------
         This function should not raise any Exception.
         """   
-        pass 
-
+        if  (type(array).__module__ == np.__name__) is False:
+            return None
+        if n <= 0 or axis == 1 and n > array.shape[1] or axis == 0 and n > array.shape[0] :
+            return None 
+        for i in range(array.shape[axis]) :
+            if i % n == 0 :
+               np.delete(array, i, axis)
+        return array 
+            
+    
     def juxtapose(self, array, n, axis):
         """
         Juxtaposes n copies of the image along the specified axis.
@@ -60,8 +90,30 @@ class ScrapBooker() :
         -------
         This function should not raise any Exception.
         """
+        ret = None
+        if  (type(array).__module__ == np.__name__) is False:
+            return ret
+        elif n > 0 and (axis == 0 or axis == 1) :
+            ret = array
+            i = 1
+            while i < n :
+                ret = np.concatenate((ret, array), axis = axis)
+                i += 1
+        return ret 
+        
+        
         pass
     def mosaic(self, array, dim):
+        
+        if  (type(array).__module__ == np.__name__)  is False:
+            return None
+       
+        if dim[0] < array.shape[0] or dim[1] < array.shape[1] :
+                return None
+        else :
+            return np.tile(array,dim) 
+
+        
         """
         Makes a grid with multiple copies of the array. The dim argument specifies
         the number of repetition along each dimensions.
@@ -77,6 +129,8 @@ class ScrapBooker() :
         -------
         This  function should not raise any Exception.
         """
+        
+        
         pass 
 
 
