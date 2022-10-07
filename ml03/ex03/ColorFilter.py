cf2378d901e76f08c1e3b8c1afc4e5233c377d28
Lucas Sehairi <lsehairi@eutelsat.com>
2022-10-07 12:17:14 +0200

    ex03 done

--- ml03/ex03/ColorFilter.py
+++ ml03/ex03/ColorFilter.py
@@ -1,4 +1,5 @@
 import numpy as np
+from ImageProcessor import ImageProcessor
 class ColorFilter :
 
 ## Iter in each el of 2d array 
@@ -6,6 +7,20 @@ class ColorFilter :
 
 ## Invert 
 # https://medium.com/analytics-vidhya/inverting-an-image-using-numpys-broadcasting-method-1f5beb7f9fa5
+
+    
+## to_[RGB]
+# https://stackoverflow.com/questions/29718877/how-to-extract-green-channel-from-rgb-image-in-python-using-scikit-image-library 
+
+
+## Celluloid 
+# https://numpy.org/doc/stable/reference/generated/numpy.linspace.html 
+
+
+## GreyScale 
+# https://www.adamsmith.haus/python/answers/how-to-convert-an-image-from-rgb-to-grayscale-in-python 
+# https://muthu.co/converting-color-images-to-grayscale-using-numpy-and-some-mathematics/
+
 
     def invert(self, array):
         """
@@ -15,18 +30,15 @@ class ColorFilter :
         array: numpy.ndarray corresponding to the image.
         Return:
         -------
-        array: numpy.ndarray corresponding to the transformed image.
+        array: numpy.nd\python\answers\how-to-convert-an-image-from-rgb-to-grayscale-in-pythonarray corresponding to the transformed image.
         None: otherwise.
         Raises:
         -------
         This function should not raise any Exception.
         """
-        ret = array.copy()
-        for x in ret :
-            for y in x :
-                for z in y :
-                    z = 255 - z
-        return ret
+        ret = array[:,:,:3]
+
+        return 1 - ret
 
 
 
@@ -109,6 +121,14 @@ class ColorFilter :
         -------
         This function should not raise any Exception.
         """
+        ret = array.copy() 
+        for el in np.linspace(0.0, 1.0, num= 50) :
+            pix = array <= el
+            ret[pix] = el
+        return ret
+
+
+
     def to_grayscale(self, array, filter, **kwargs):
         """
         Applies a grayscale filter to the image received as a numpy array.
@@ -128,11 +148,28 @@ class ColorFilter :
         -------
         This function should not raise any Exception.
         """  
+        weight = kwargs.get('weights')
+        if filter == 'weight' or filter == 'w' :
+            return np.dot(array[..., :3], weight)
+        elif filter == 'mean' or filter == 'm' :
+            mean = np.sum(array, axis=-1) / 3 
+            return np.broadcast_to(mean[..., None], array.shape) 
+        else : return None 
+
     
 
 if __name__ == "__main__":
 
     imp = ImageProcessor()
-    arr = imp.load("assets/42AI.png")
-    # Output :
-    # Loading image of dimensions 200 x 200
+    arr = imp.load("elon_canaGAN.png")
+
+
+    imp.display(arr)
+    cf = ColorFilter()
+    imp.display(cf.invert(arr))
+    imp.display(cf.to_green(arr))
+    imp.display(cf.to_red(arr))
+    imp.display(cf.to_blue(arr))
+
+    imp.display(cf.to_grayscale(arr, 'm'))
+    imp.display(cf.to_grayscale(arr, 'weight', weights = [0.2, 0.3, 0.5]))
