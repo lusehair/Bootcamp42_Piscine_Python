4fe66b583181af11b0640694db95e8b076a07cdb
Lucas SEHAIRI <lusehair@e1r8p10.clusters.42paris.fr>
2022-10-12 21:49:41 +0200

    Bordel

--- ml03/ex04/Kmeans.py
+++ ml03/ex04/Kmeans.py
@@ -1,138 +1,131 @@
-import numpy as np 
-import sys 
+
+
+import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-
-
-
-# https://www.lovelyanalytics.com/2016/09/06/k-means-comment-ca-marche/ 
-# https://stackoverflow.com/questions/3518778/how-do-i-read-csv-data-into-a-record-array-in-numpy
-# https://stackoverflow.com/questions/39390418/python-how-can-i-enable-use-of-kwargs-when-calling-from-command-line-perhaps
-# https://github.com/SKTPausanias/python_bootcamp-42AI/blob/main/day03/ex04/Kmeans.py
-
+import math
+import sys
 
 class KmeansClustering:
-    def __init__(self, max_iter=20, ncentroid=5):
-        self.ncentroid = ncentroid # number of centroids
-        self.max_iter = max_iter # number of max iterations to update the centroids
-        self.centroids = [] # values of the centroids
-        self.array = None 
+	def __init__(self, max_iter=10, ncentroid=4):
+		self.ncentroid = ncentroid 
+		self.max_iter = max_iter 
+		self.centroids = [] 
 
-    def fit(self, X):
-        """
-        Run the K-means clustering algorithm.
-        For the location of the initial centroids, random pick ncentroids from the dataset.
-        Args:
-        -----
-        X: has to be an numpy.ndarray, a matrice of dimension m * n.
-        Return:
-        -------
-        None.
-        Raises:
-        -------
-        This function should not raise any Exception.
-        """
+	def euclidean_distance(self, a, b):
+		return math.sqrt(sum(np.power(b - a, 2)))
+	
+	def fit(self, X):
+		"""
+		Run the k-means clustering algorithm.
+		For the location of the initial centroids, random pick ncentroids from the dataset.
+		Args:
+			X: has to be an numpy.ndarray, a matrice of dimension m * n.
+		Returns:
+			None.
+		Raises:
+			This function should not raise any Exception.
+		"""
+		mean = np.mean(X, axis = 0)
+		std = np.std(X, axis = 0)
+		n = X.shape[0]
+		c = X.shape[1]
+		self.centroids = np.random.randn(self.ncentroid, c)*std + mean
+		dotcolor = 10*["r", "g", "c", "b", "k"]
+		graph=plt.figure()
+		draw=Axes3D(graph)
 
-        # centroids = X.copy() 
-        # np.random.shuffle(centroids)
-        # self.centroids = centroids[:self.ncentroid]
-        # plt.scatter(X[:,0], X[:,1])
-        # plt.scatter(self.centroids[:,0], self.centroids[:,1], c='r', s=100)
-        # distances = np.sqrt(((X - centroids[:, np.newaxis])**2).sum(axis=2)) 
-        # deminor = np.argmin(distances, axis=0) 
+		for el in range(X.shape[0]) :
+			dot = dotcolor[4] 
+			draw.scatter(X[el][0], X[el][1], X[el][2], color = dot)
+		for i, el in enumerate(self.centroids) :
+			dot = dotcolor[i] 
+			draw.scatter(el[0], el[1], el[2], s=130, color = dot, marker= "x")
 
-        # INIT VISUALIZATION 
-        self.centroids = np.random.randn(self.ncentroid, X.shape[1]) * np.std(X, axis=0) + np.mean(X, axis=0)
-        graph = plt.figure()
-        dotcolor = 10*["g", "c", "k", "r", "b"]
-        draw = Axes3D(graph) 
-        for el in range(X.shape[0]) :
-            dot = dotcolor[4] 
-            draw.scatter(X[el][0], X[el][1], color = dot)
-        for i, el in enumerate(self.centroids) :
-            dot = dotcolor[i] 
-            draw.scatter(el[0], el[1], el[2], color = dot, marker= "x")
-        plt.show() 
-        
-        dist = np.zeros(X.shape[0], self.ncentroid)
-        self.clustering = np.zeros(X.shape[0])
-        
-        # Getting the mean 
-        for i in range(self.max_iter) :
-            for i in range(self.ncentroid) :
-                self.dist[:,i] = np.linalg.norm(X - self.centroids[i], axis=1)
-            self.clustering = np.argmin(dist, axis = 1) 
-
-            # Affinage 
-            for i in range(self.ncentroid): 
-                self.centroids[i] = np.mean(X[self.clustering == i], axis = 0)
-        self.table = np.zeros(X.shape[0], X.shape[1] + 1) # + 1 not needed ?
-        self.table[:,:-1] = X
-        self.table[:,3] = self.clustering 
-
-    def predict(self, X):
-        """
-        Predict from wich cluster each datapoint belongs to.
-        Args:
-        -----
-        X: has to be an numpy.ndarray, a matrice of dimension m * n.
-        Return:
-        -------
-        the prediction has a numpy.ndarray, a vector of dimension m * 1.
-        Raises:
-        -------
-        This function should not raise any Exception.
-        """
-        y = [self.table[self.table[:,3]==k] for k in np.unique(self.table[:,3])]
-        moy = np.zeros((self.ncentroid, X.shape[1] + 1))
-        for i, el in enumerate(y) :
-            moy[i] = el.mean(axis=0) 
-        moy = moy[moy[:, 0].argsort()]
-        ret = np.chararray(shape=(X.shape[0], 1))
-        for el in range(X.shape[0]):
-            if self.table[i][3] == moy[0][3]:
-                ret[i] = "The flying cities of Venus,"
-            elif self.table[i][3] == moy[1][3]:
-                ret[i] = "United Nations of Earth"
-            elif self.table[i][3] == moy[2][3] :
-                ret[i] = "Mars Republic,"
-            elif self.table[i][3] == moy[3][3] :
-                ret[i] = "Asteroids' Belt colonies."
-        return ret
-
-    def drawing(self) :
-
-        dotcolors = 10*["g", "c", "k", "r", "b"]
-        graph = plt.figure()
-        draw = Axes3D(graph)
-
-        for el in range(self.table.shape[0]):
-            dot = dotcolors[int(self.table[el][3])] 
-            draw.scatter(self.table[el][0], self.table[el][1], self.table[el][2], color = dot)
-        
-        for i, value in enumerate(self.centroids) :
-            dot = dotcolors[i] 
-            draw.scatter(value[0], value[1], value[2], s = 130, color = dot, marker= "x")
-        plt.show()
-            
-    
+		plt.show()
 
 
+		dist = np.zeros((n, self.ncentroid))
+		self.clusters = np.zeros(X.shape[0])
+
+		# Getting the mean 
+		for i in range(self.max_iter) :
+			for i in range(self.ncentroid) :
+				dist[:,i] = np.linalg.norm(X - self.centroids[i], axis=1)
+		self.clusters = np.argmin(dist, axis = 1) 
 
 
+		for i in range(self.ncentroid):
+			self.centroids[i] = np.mean(X[self.clusters == i], axis=0)
+
+		self.table = np.zeros((X.shape[0], X.shape[1] + 1))
+		self.table[:,:-1] = X
+		self.table[:,3] = self.clusters
+	
+	def predict(self, X):
+		"""
+		Predict from wich cluster each datapoint belongs to.
+		Args:
+			X: has to be an numpy.ndarray, a matrice of dimension m * n.
+		Returns:
+			the prediction has a numpy.ndarray, a vector of dimension m * 1.
+		Raises:
+			This function should not raise any Exception.
+		"""
+
+		y = [self.table[self.table[:,3]==kluster] for kluster in np.unique(self.table[:,3])]
+		moy = np.zeros((self.ncentroid, X.shape[1] + 1))
+		i = 0
+
+		for i, el in enumerate(y) :
+			moy[i] = el.mean(axis=0) 
+		moy = moy[moy[:, 0].argsort()]
+		
+		ret = np.chararray(shape=(X.shape[0], 1), itemsize=26)
+		for i in range(X.shape[0]):
+			if (self.table[i][3] == moy[0][3]):
+				ret[i] = "The flying cities of Venus"
+			elif (self.table[i][3] == moy[1][3]):
+				ret[i] = "United Nations of Earth"
+			elif (self.table[i][3] == moy[2][3]):
+				ret[i] = "Mars Republic"
+			elif (self.table[i][3] == moy[3][3]):
+				ret[i] = "Asteroids Belt colonies"
+		return ret
 
 
-def main(**kwargs) : 
-    ret = KmeansClustering()
-    for key, value in kwargs.items() :
-        setattr(ret, key, value) 
-    return ret  
+def main(**kwargs):
+	
+	
+	
+	f = open("solar_system_census.csv")
+	
+	kluster = KmeansClustering(max_iter=100)
+	for k, v in kwargs.items():
+		print('keyword argument: {} = {}'.format(k, v))
+	lines = f.readlines()
+	data = []
+	for i in range(len(lines)):
+		data.append(lines[i].split(','))
+	dataset = np.delete(np.array(data[1:], dtype='float'), 0, 1)
+	dataset = dataset / np.linalg.norm(dataset, axis = 0)
 
+	kluster.fit(dataset)
+	print(kluster.predict(dataset))
 
+	colors = 10*["r", "g", "c", "b", "k"]
+	fig=plt.figure()
+	ax=Axes3D(fig)
+	for i in range(kluster.table.shape[0]):
+		color = colors[int(kluster.table[i][3])]
+		ax.scatter(kluster.table[i][0],kluster.table[i][1],kluster.table[i][2], color = color)
+	i = 0
+	for centroid in kluster.centroids:
+		color = colors[i]
+		ax.scatter(centroid[0], centroid[1], centroid[2], s = 130 , color = color, marker = "x")
+		i += 1
+	plt.show()
 
 if __name__ == "__main__":
-
-    cluster = main(**dict(arg.split('=') for arg in sys.argv[1:])) 
-    array = np.genfromtxt(cluster.filepath,delimiter=',')
-
-
+    main(
+         **dict(arg.split('=') for arg in sys.argv[1:])) # kwargs
---
+++ ml03/ex04/old.py
@@ -0,0 +1,185 @@
+import numpy as np 
+import sys 
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import math
+import random
+
+
+# https://www.lovelyanalytics.com/2016/09/06/k-means-comment-ca-marche/ 
+# https://stackoverflow.com/questions/3518778/how-do-i-read-csv-data-into-a-record-array-in-numpy
+# https://stackoverflow.com/questions/39390418/python-how-can-i-enable-use-of-kwargs-when-calling-from-command-line-perhaps
+# https://github.com/SKTPausanias/python_bootcamp-42AI/blob/main/day03/ex04/Kmeans.py
+
+
+class KmeansClustering:
+    def __init__(self, max_iter=20, ncentroid=5):
+        self.ncentroid = ncentroid # number of centroids
+        self.max_iter = max_iter # number of max iterations to update the centroids
+        self.centroids = [] # values of the centroids
+        self.array = None 
+
+    def fit(self, X):
+        """
+        Run the K-means clustering algorithm.
+        For the location of the initial centroids, random pick ncentroids from the dataset.
+        Args:
+        -----
+        X: has to be an numpy.ndarray, a matrice of dimension m * n.
+        Return:
+        -------
+        None.
+        Raises:
+        -------
+        This function should not raise any Exception.
+        """
+
+        # centroids = X.copy() 
+        # np.random.shuffle(centroids)
+        # self.centroids = centroids[:self.ncentroid]
+        # plt.scatter(X[:,0], X[:,1])
+        # plt.scatter(self.centroids[:,0], self.centroids[:,1], c='r', s=100)
+        # distances = np.sqrt(((X - centroids[:, np.newaxis])**2).sum(axis=2)) 
+        # deminor = np.argmin(distances, axis=0) 
+
+        # INIT VISUALIZATION 
+        
+        # mean = np.mean(X, axis = 0)
+        # std = np.std(X, axis = 0)
+        # n = X.shape[0]
+        # c = X.shape[1]
+        # self.centroids = np.random.randn(self.ncentroid, c) * std + mean
+        # graph = plt.figure()
+        # dotcolor = 10*["g", "c", "k", "r", "b"]
+        # draw = Axes3D(graph) 
+        # for el in range(X.shape[0]) :
+        #     dot = dotcolor[4] 
+        #     draw.scatter(X[el][0], X[el][1], color = dot)
+        # for i, el in enumerate(self.centroids) :
+        #     dot = dotcolor[i] 
+        #     draw.scatter(el[0], el[1], el[2], color = dot, marker= "x")
+        # plt.show() 
+        
+        # dist = np.zeros(X.shape[0], self.ncentroid)
+        # self.clustering = np.zeros(X.shape[0])
+        
+        # # Getting the mean 
+        # for i in range(self.max_iter) :
+        #     for i in range(self.ncentroid) :
+        #         self.dist[:,i] = np.linalg.norm(X - self.centroids[i], axis=1)
+        #     self.clustering = np.argmin(dist, axis = 1) 
+
+        #     # Affinage 
+        #     for i in range(self.ncentroid): 
+        #         self.centroids[i] = np.mean(X[self.clustering == i], axis = 0)
+        # self.table = np.zeros(X.shape[0], X.shape[1] + 1) # + 1 not needed ?
+        # self.table[:,:-1] = X
+        # self.table[:,3] = self.clustering 
+        mean = np.mean(X, axis = 0)
+        std = np.std(X, axis = 0)
+        n = X.shape[0]
+        c = X.shape[1]
+        self.centroids = np.random.randn(self.ncentroid, c)*std + mean
+        #centers_old = np.zeros(self.centroids.shape) # to store old centers
+        #centers_new = deepcopy(self.centroids)
+        #error = np.linalg.norm(centers_new - centers_old)
+        colors = 10*["r", "g", "c", "b", "k"]
+        fig=plt.figure()
+        ax=Axes3D(fig)
+        for i in range(X.shape[0]):
+            color = colors[4]
+            ax.scatter(X[i][0],X[i][1],X[i][2], color = color)
+        i = 0
+        for centroid in self.centroids:
+            color = colors[i]
+            ax.scatter(centroid[0], centroid[1], centroid[2], s = 130 , color = color, marker = "x")
+            i += 1
+        plt.show()
+        self.clusters = np.zeros(n)
+        distances = np.zeros((n, self.ncentroid))
+        for i in range(self.max_iter):
+            for i in range(self.ncentroid):
+                distances[:,i] = np.linalg.norm(X - self.centroids[i], axis=1)
+            self.clusters = np.argmin(distances, axis = 1)
+            #centers_old = deepcopy(centers_new)
+            #recalculate centroids
+            for i in range(self.ncentroid):
+                self.centroids[i] = np.mean(X[self.clusters == i], axis=0)
+            #error = np.linalg.norm(centers_new - centers_old)
+            #if error == 0:
+            #	break
+        self.table = np.zeros((X.shape[0], X.shape[1] + 1))
+        self.table[:,:-1] = X
+        self.table[:,3] = self.clusters
+	
+
+    def predict(self, X):
+        """
+        Predict from wich cluster each datapoint belongs to.
+        Args:
+        -----
+        X: has to be an numpy.ndarray, a matrice of dimension m * n.
+        Return:
+        -------
+        the prediction has a numpy.ndarray, a vector of dimension m * 1.
+        Raises:
+        -------
+        This function should not raise any Exception.
+        """
+        y = [self.table[self.table[:,3]==k] for k in np.unique(self.table[:,3])]
+        moy = np.zeros((self.ncentroid, X.shape[1] + 1))
+        for i, el in enumerate(y) :
+            moy[i] = el.mean(axis=0) 
+        moy = moy[moy[:, 0].argsort()]
+        ret = np.chararray(shape=(X.shape[0], 1))
+        for el in range(X.shape[0]):
+            if self.table[i][3] == moy[0][3]:
+                ret[i] = "The flying cities of Venus,"
+            elif self.table[i][3] == moy[1][3]:
+                ret[i] = "United Nations of Earth"
+            elif self.table[i][3] == moy[2][3] :
+                ret[i] = "Mars Republic,"
+            elif self.table[i][3] == moy[3][3] :
+                ret[i] = "Asteroids' Belt colonies."
+        return ret
+
+    def drawing(self) :
+
+        dotcolors = 10*["g", "c", "k", "r", "b"]
+        graph = plt.figure()
+        draw = Axes3D(graph)
+
+        for el in range(self.table.shape[0]):
+            dot = dotcolors[int(self.table[el][3])] 
+            draw.scatter(self.table[el][0], self.table[el][1], self.table[el][2], color = dot)
+        
+        for i, value in enumerate(self.centroids) :
+            dot = dotcolors[i] 
+            draw.scatter(value[0], value[1], value[2], s = 130, color = dot, marker= "x")
+        plt.show()
+            
+    
+
+
+
+
+
+
+def main(**kwargs) : 
+    ret = KmeansClustering()
+    for key, value in kwargs.items() :
+        setattr(ret, key, value) 
+    return ret  
+
+
+
+if __name__ == "__main__":
+
+    cluster = main(**dict(arg.split('=') for arg in sys.argv[1:])) 
+    array = np.genfromtxt(cluster.filepath,delimiter=',')
+    cluster.fit(array)
+    print(cluster.predict(array)) 
+    cluster.drawing()
+
+
+
